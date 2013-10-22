7e5d7b9df19cb9cbaebee0defa265ad16893f683
Donatello <donald.nguyen@blackbaud.com>
2013-10-22 17:00:55 -0500

    test exit_code

--- lib/watirmark/at_exit.rb
+++ lib/watirmark/at_exit.rb
@@ -1,12 +1,14 @@
 module Watirmark
   def self.add_exit_task
     at_exit {
+      puts "INSIDE EXIT CODE"
       if $!.nil? || $!.is_a?(SystemExit) && $!.success?
         code = 0
       else
         code = $!.is_a?(SystemExit) ? $!.status : 1
       end
       yield if block_given?
+      puts "EXIT CODE IS #{code}"
       exit code
     }
   end
