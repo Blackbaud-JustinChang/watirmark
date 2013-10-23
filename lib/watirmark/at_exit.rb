8e77e29bf644b06c61df63027a628bb8ddffc8ed
Donatello <donald.nguyen@blackbaud.com>
2013-10-23 14:33:17 -0500

    just spec helper

--- lib/watirmark/at_exit.rb
+++ lib/watirmark/at_exit.rb
@@ -1,23 +1,23 @@
 module Watirmark
   def self.add_exit_task
-    at_exit {
-      puts "INSIDE EXIT CODE"
-      puts "$! = #{$!.inspect}"
-      puts "$!.nil? = #{$!.nil?}"
-      puts "$!.is_a?(SystemExit) = #{$!.is_a?(SystemExit)}"
-      puts "$!.success? = #{$!.success?}" if $!.is_a?(SystemExit)
-      if $!.nil? || $!.is_a?(SystemExit) && $!.success?
-        code = 0
-      else
-        code = $!.is_a?(SystemExit) ? $!.status : 1
-      end
-      yield if block_given?
-      puts "EXIT CODE IS #{code}"
-      exit code
-    }
-  rescue Exception => e
-    puts "THERE WAS AN EXIT ISSUE"
-    puts e.inspect
-    puts e.backtrace.join "\n"
+  #  at_exit {
+  #    puts "INSIDE EXIT CODE"
+  #    puts "$! = #{$!.inspect}"
+  #    puts "$!.nil? = #{$!.nil?}"
+  #    puts "$!.is_a?(SystemExit) = #{$!.is_a?(SystemExit)}"
+  #    puts "$!.success? = #{$!.success?}" if $!.is_a?(SystemExit)
+  #    if $!.nil? || $!.is_a?(SystemExit) && $!.success?
+  #      code = 0
+  #    else
+  #      code = $!.is_a?(SystemExit) ? $!.status : 1
+  #    end
+  #    yield if block_given?
+  #    puts "EXIT CODE IS #{code}"
+  #    exit code
+  #  }
+  #rescue Exception => e
+  #  puts "THERE WAS AN EXIT ISSUE"
+  #  puts e.inspect
+  #  puts e.backtrace.join "\n"
   end
 end
